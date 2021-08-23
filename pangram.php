f87ad01b21ba0c9c814b64d7bb5e66b7cff7ee82
apitongcm <apitongcm@gmail.com>
2021-08-23 22:31:03 +0800

    pangram js

---
+++ pangram.php
@@ -0,0 +1,15 @@
+//pangram also called holoalphabetic sentense is a sentence using every letter given alphabet at once.
+// this function will be able to distinguish if the phrase or sentence you input is a pangram or not. 
+
+
+function detect_pangram($string) { 
+  #your code here
+    $input = strtolower(trim($string));
+    $characters = str_split("thequickbrownfoxjumpsoverthelazydog");
+  
+    foreach($characters as $character){
+        if(!strstr($input, $character))
+          return false;
+    }
+        return true;
+  }
